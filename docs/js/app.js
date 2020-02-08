9b83c8d36b6b8548c37f2006f3fff1e746c36b59
lennartdeknikker <me@lenn4rt.com>
2020-02-08 18:29:24 +0100

    :sparkles: add data to dom

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,66 +1,29 @@
-async function getDataFor(data) {
-  // endpoint and request header configuration
-  const endpoint = 'https://api.spacexdata.com/v3/';
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  // fetch request returning obtained data.
-  return fetch(`${endpoint + data}`, requestOptions)
-    .then(handleResponse)
-    .then(data => {console.log(data); return data;})
-    .catch(error => console.log(error))
+import { getDataFor } from './utilities.js'
 
-// Error handling as learned from https://css-tricks.com/using-fetch/
-
-  function handleResponse(response) {
-    let contentType = response.headers.get('content-type')
-    if (contentType.includes('application/json')) {
-      return handleJSONResponse(response)
-    } else if (contentType.includes('text/html')) {
-      return handleTextResponse(response)
-    } else {
-      throw new Error(`Sorry, content-type ${contentType} is not supported.`)
-    }
-  }
-
-  function handleJSONResponse (response) {
-    return response.json()
-    .then(json => {
-      if (response.ok) {
-        return json
-      } else {
-        return Promise.reject(Object.assign({}, json, {
-          status: response.status,
-          statusText: response.statusText
-        }))
-      }
-    })
-  }
-
-  function handleTextResponse (response) {
-    return response.text()
-    .then(text => {
-      if (response.ok) {
-        return text
-      } else {
-        return Promise.reject({
-          status: response.status,
-          statusText: response.statusText,
-          err: text
-        })
-      }
-    })
-  }
-}
-
-async function showData() {
-  await getDataFor('launches').then(
+async function showDataFor(identifier, parentElement, childElementType = 'li') {
+  await getDataFor(identifier).then(
     data => {
-      document.querySelector('#hallo').innerHTML = data[0].mission_name;
+      if (data.length > 1) {
+        for (let i = 0; i < data.length; i++) {
+          let newNode = document.createElement(childElementType);
+          let newContent = document.createTextNode(data[i].mission_name);
+          newNode.appendChild(newContent);
+          let target = document.querySelector(parentElement);
+          target.appendChild(newNode);
+        }
+      } else {
+        let newNode = document.createElement(childElementType);
+        let newContent = document.createTextNode(data.mission_name);
+        newNode.appendChild(newContent);
+        let target = document.querySelector(parentElement);
+        target.appendChild(newNode);
+      }
     }
   )
 }
 
-showData();
+showDataFor('launches/past', '.past-launches');
+showDataFor('launches/upcoming', '.upcoming-launches');
+showDataFor('launches/latest', '.latest-launch', 'p');
+showDataFor('launches/next', '.next-launch', 'p');
+
---
+++ docs/js/utilities.js
@@ -0,0 +1,58 @@
+async function getDataFor(data) {
+  // endpoint and request header configuration
+  const endpoint = 'https://api.spacexdata.com/v3/';
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  // fetch request returning obtained data.
+  return fetch(`${endpoint + data}`, requestOptions)
+    .then(handleResponse)
+    .then(data => {console.log(data); return data;})
+    .catch(error => console.log(error))
+
+// Error handling as learned from https://css-tricks.com/using-fetch/
+
+  function handleResponse(response) {
+    let contentType = response.headers.get('content-type')
+    if (contentType.includes('application/json')) {
+      return handleJSONResponse(response)
+    } else if (contentType.includes('text/html')) {
+      return handleTextResponse(response)
+    } else {
+      throw new Error(`Sorry, content-type ${contentType} is not supported.`)
+    }
+  }
+
+  function handleJSONResponse (response) {
+    return response.json()
+    .then(json => {
+      if (response.ok) {
+        return json
+      } else {
+        return Promise.reject(Object.assign({}, json, {
+          status: response.status,
+          statusText: response.statusText
+        }))
+      }
+    })
+  }
+
+  function handleTextResponse (response) {
+    return response.text()
+    .then(text => {
+      if (response.ok) {
+        return text
+      } else {
+        return Promise.reject({
+          status: response.status,
+          statusText: response.statusText,
+          err: text
+        })
+      }
+    })
+  }
+}
+
+export { getDataFor };
