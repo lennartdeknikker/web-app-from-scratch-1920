2a7c6ca2b97511c23073b2631509af8ba859766c
lennartdeknikker <me@lenn4rt.com>
2020-02-25 10:19:26 +0100

    add detail view info

--- docs/js/components/detailview.js
+++ docs/js/components/detailview.js
@@ -4,7 +4,6 @@ import Utilities from '../utilities.js';
 const detailView = {
   currentPosition: 0,
   init(flightNumber, data) {
-    console.log(data);
     const selectedItem = document.querySelector(`.flight-${flightNumber}`);
     const launchList = document.querySelector('.launches-list');
     let newPosition = Array.prototype.indexOf.call(launchList.children, selectedItem) + 1;
@@ -15,35 +14,70 @@ const detailView = {
         roundTo4(newPosition);
       }
     }
+
     roundTo4(newPosition);
     if (newPosition > launchList.childElementCount) {
       newPosition = launchList.childElementCount - 1;
     }
+
     const targetElement = document.querySelectorAll('.launches-list-item')[newPosition - 1];
+    const detailviewTitle = Utilities.createNewElement('h2', 'detailview-title', data.mission_name);
 
     if (newPosition !== detailView.currentPosition) {
       Utilities.removeAll('.detailview');
-      const newDiv = Utilities.createNewElement('div', 'detailview');
-      newDiv.innerHTML = data.mission_name;
-      targetElement.insertAdjacentElement('afterend', newDiv);
-    } else document.querySelector('.detailview').innerHTML = data.mission_name;
+      const newDiv = Utilities.createNewElement(
+        'div',
+        'detailview',
+      );
+      newDiv.appendChild(detailviewTitle);
+      targetElement.insertAdjacentElement(
+        'afterend',
+        newDiv,
+      );
+    } else {
+      document.querySelector('.detailview-title').remove();
+      document.querySelector('.detailview').appendChild(detailviewTitle);
+    }
+
     detailView.currentPosition = newPosition;
-    this.renderHtml(data);
+
+    this.renderHtml(data, '.detailview');
   },
-  renderHtml(data) {
+  renderHtml(rawData, targetElement) {
+    if (document.querySelector('.property-list')) document.querySelector('.property-list').remove();
     const newUl = Utilities.createNewElement('ul', 'property-list');
-    for (const property in data) {
-      if (data[property] !== null && typeof data[property] !== 'object') {
-        const newLi = Utilities.createNewElement('li', 'property', `${Utilities.capitalize(property.replace(/_/g, ' '))}: ${data[property]}`);
-        newUl.appendChild(newLi);
-      } else if (data[property] !== null && typeof data[property] === 'object') {
-        data[property].forEach((element) => {
-          const newLi = Utilities.createNewElement('li', 'property', `${Utilities.capitalize(property.replace(/_/g, ' '))}: ${data[property]}`);
+    function addDataToList(data) {
+      for (const property in data) {
+        if (data[property] !== null && typeof data[property] !== 'object') {
+          const propertySpan = Utilities.createNewElement(
+            'span',
+            'detail-property',
+            `${Utilities.capitalize(property.replace(/_/g, ' '))}: `,
+          );
+          const detailSpan = Utilities.createNewElement(
+            'span',
+            'detail-data',
+            `${data[property]}`,
+          );
+          const newLi = Utilities.createNewElement(
+            'li',
+            'detail',
+            '',
+            propertySpan,
+          );
+
+          newLi.appendChild(detailSpan);
           newUl.appendChild(newLi);
-        });
+        } else
+
+        if (data[property] !== null && typeof data[property] === 'object') {
+          addDataToList(data[property]);
+        }
       }
     }
-    Utilities.appendElement('.launches-list', newUl);
+    addDataToList(rawData);
+
+    Utilities.appendElement(targetElement, newUl);
   },
 };
 
